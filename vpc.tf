b40e51fe6e9522d0194729b0f14fe1ce52e09d47
Abdimalik Mao <58399886+AMao7@users.noreply.github.com>
2021-08-03 14:02:43 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "ELK-vpc" {
-    cidr_block = "10.0.0.0/16" #cidr_block: 10.0.1.0/24. We have 254 IP addresses in this subnet
+    cidr_block = "10.0.0.0/16" # 254 IP addresses in this subnet
     enable_dns_support = "true" #gives you an internal domain name
     enable_dns_hostnames = "true" #gives you an internal host name
     enable_classiclink = "false"
@@ -13,7 +13,7 @@ resource "aws_vpc" "ELK-vpc" {
 resource "aws_subnet" "ELK-subnet-public" {
     vpc_id = "${aws_vpc.ELK-vpc.id}"
     cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = "true" //it makes this a public subnet
+    map_public_ip_on_launch = "true" # it makes this a public subnet
     availability_zone = "eu-west-2a"
     tags = {
         Name = "ELK-subnet-public"
